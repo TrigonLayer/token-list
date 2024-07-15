7a8af345a0d32da23ee81a149654131c67adaf1b
Excoinsevm <elgatocoin@gmail.com>
2024-07-15 20:03:48 +0000

    first commt

--- src/buildList.js
+++ src/buildList.js
@@ -1,19 +1,5 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
-const optimism = require("./tokens/optimism.json");
-const celo = require("./tokens/celo.json");
-const arbitrum = require("./tokens/arbitrum.json");
-const bnb = require("./tokens/bnb.json");
-const sepolia = require("./tokens/sepolia.json");
-const avalanche = require("./tokens/avalanche.json");
-const base = require("./tokens/base.json");
-const blast = require("./tokens/blast.json");
 
 const bridgeUtils = require("@uniswap/token-list-bridge-utils");
 
@@ -32,20 +18,6 @@ module.exports = function buildList() {
     keywords: ["uniswap", "default"],
     tokens: [
       ...mainnet,
-      ...ropsten,
-      ...goerli,
-      ...kovan,
-      ...rinkeby,
-      ...polygon,
-      ...mumbai,
-      ...optimism,
-      ...celo,
-      ...arbitrum,
-      ...bnb,
-      ...sepolia,
-      ...avalanche,
-      ...base,
-      ...blast,
     ]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
